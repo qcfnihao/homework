60e73da79e114eb6e2e6ace06d9fae9e8cce6ca2
qcfnihao <1142692845@qq.com>
2019-04-03 14:57:12 +0800

    DI基本流程

--- springTest/src/main/java/spring/test/Student.java
+++ springTest/src/main/java/spring/test/Student.java
@@ -3,8 +3,10 @@ package spring.test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
+
 @Component
-public class Student {
+public class Student implements Serializable {
 
     @Autowired
     private Teacher teacher;
